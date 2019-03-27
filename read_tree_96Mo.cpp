2bfd7831c046e367c806b06068a95e138eded59c
cecgresonant <a.c.larsen@fys.uio.no>
2019-03-27 11:38:37 +0100

    Updated read_tree_96Mo.cpp - this works, but still missing bananas

--- read_tree_96Mo.cpp
+++ read_tree_96Mo.cpp
@@ -25,7 +25,6 @@
 #include <stdio.h>
 #include <string>
 #define MAX_HITS_EDET 8    //!< Maximum number of hits in e detectors per event
-#define MAX_HITS_DEDET 64    //!< Maximum number of hits in de detectors per event
 #define MAX_HITS_LABR 64    //!< Maximum number of hits in labr detectors per event (can be more than 30 due to the long time range for the vaidation signal)
 
 void read_tree_96Mo(){
@@ -52,10 +51,10 @@ void read_tree_96Mo(){
     // Variables for detector IDs, timestamps, energies etc
     Int_t deDet_ID=0, tel_ID=0;
     Long64_t deDet_timestamp=0;
-    Double_t deDet_energy;
+    Double_t deDet_energy; 
     
     Int_t deDet_mult=0, eDet_mult=0;
-    Double_t eDet_time[MAX_HITS_EDET], eDet_energy[MAX_HITS_EDET];//, deDet_energy[MAX_HITS_DEDET];
+    Double_t eDet_time[MAX_HITS_EDET], eDet_energy[MAX_HITS_EDET];
     
     Int_t labr_mult=0, labr_ID[MAX_HITS_LABR], labr_ring[MAX_HITS_LABR];
     Double_t labr_time[MAX_HITS_LABR], labr_energy[MAX_HITS_LABR];
@@ -79,10 +78,6 @@ void read_tree_96Mo(){
         eDet_time[i]=0;
     }
 
-    //for (int i=0; i<MAX_HITS_DEDET; ++i) {
-      //  deDet_energy[i]=0;
-    //}
-
     for (int i=0; i<MAX_HITS_LABR; ++i) {
         labr_ID[i]=0;
         labr_energy[i]=0;
@@ -93,34 +88,33 @@ void read_tree_96Mo(){
     // Set branch addresses in the TChain
     t->SetBranchAddress("deDet_ID", &deDet_ID); // Front detector ID
     t->SetBranchAddress("tel_ID", &tel_ID); // Back detector ID
-    t->SetBranchAddress("deDet_energy", &deDet_energy);
-    t->SetBranchAddress("deDet_timestamp", &deDet_timestamp);
+    t->SetBranchAddress("deDet_energy", &deDet_energy); // Delta E energy
+    t->SetBranchAddress("deDet_timestamp", &deDet_timestamp); // Delta E timestamp
     
-    t->SetBranchAddress("eDet_mult", &eDet_mult);
-    t->SetBranchAddress("eDet_energy", eDet_energy);
-    t->SetBranchAddress("eDet_time", eDet_time);
+    t->SetBranchAddress("eDet_mult", &eDet_mult);   // E multiplicity
+    t->SetBranchAddress("eDet_energy", eDet_energy); // E energy
+    t->SetBranchAddress("eDet_time", eDet_time); // E time
     
-    t->SetBranchAddress("labr_mult", &labr_mult);
-    t->SetBranchAddress("labr_ID", labr_ID);
-    t->SetBranchAddress("labr_ring", labr_ring);
-    t->SetBranchAddress("labr_energy", labr_energy);
-    t->SetBranchAddress("labr_time", labr_time);
+    t->SetBranchAddress("labr_mult", &labr_mult); // LaBr3 multiplicity
+    t->SetBranchAddress("labr_ID", labr_ID);    // LaBr3 ID
+    t->SetBranchAddress("labr_ring", labr_ring);    // LaBr3 ring (6 different theta angles)
+    t->SetBranchAddress("labr_energy", labr_energy); // LaBr3 energy
+    t->SetBranchAddress("labr_time", labr_time);    // LaBr3 time
     
     // Define matrices and histograms
     const int max_e = 20000, max_de = 10000; // Max energies in the E and Delta E detectors
     const int max_e_labr3 =  20000; // Max energy for the LaBr3 detectors
     
-    TH1D *h_deDet_mult = new TH1D("h_eDet_mult","#Delta E_{front} multiplicity",64,0,64);
-    TH1D *h_eDet_mult = new TH1D("h_eDet_mult","E_{back} multiplicity",8,0,8);
+    TH1D *h_eDet_mult = new TH1D("h_eDet_mult","E_{back} multiplicity",9,0,9);
     TH1D *h_LaBr3_mult = new TH1D("h_LaBr3_mult","LaBr3 multiplicity",60,0,60); // Potentially more hits than 30, because the validation signal gives a 2 us time wndow
 
-    TH2D *h_deDet_energy = new TH2D("h_deDet_energy","#Delta E_{front} energy",1000,0,max_de,64,0,64);
-    TH2D *h_eDet_energy = new TH2D("h_eDet_energy","E_{back} energy",1000,0,max_e,8,0,8);
-    TH2D *h_LaBr3_energy = new TH2D("h_LaBr3_energy","LaBr3 energy",40000,0,max_e_labr3,30,0,30);
+    TH2D *h_deDet_energy = new TH2D("h_deDet_energy","#Delta E_{front} energy",1000,0,max_de,65,0,65);
+    TH2D *h_eDet_energy = new TH2D("h_eDet_energy","E_{back} energy",1000,0,max_e,9,0,9);
+    TH2D *h_LaBr3_energy = new TH2D("h_LaBr3_energy","LaBr3 energy",20000,0,max_e_labr3,32,0,32);
 
-    TH2D *h_deDet_time = new TH2D("h_deDet_time","#Delta E_{front} time",20000,-1000,1000,8,0,8);
-    TH2D *h_eDet_time = new TH2D("h_eDet_time","E_{back} time",20000,-1000,1000,8,0,8);
-    TH2D *h_LaBr3_time = new TH2D("h_LaBr3_time","LaBr3 time",20000,-1000,1000,30,0,30);
+    TH2D *h_deDet_time = new TH2D("h_deDet_time","#Delta E_{front} time",20000,-1000,1000,65,0,65);
+    TH2D *h_eDet_time = new TH2D("h_eDet_time","E_{back} time",20000,-1000,1000,9,0,9);
+    TH2D *h_LaBr3_time = new TH2D("h_LaBr3_time","LaBr3 time",20000,-1000,1000,32,0,32);
 
     // Cecilie, modification 22 March 2019:
     // Making Delta E - E matrices as in the old user_sort.cpp by Alexander Bürger
@@ -148,13 +142,11 @@ void read_tree_96Mo(){
     //TH2D *h_Ex_gamma_with_bg = new TH2D("h_Ex_gamma_with_bg","Excitation Energy vs gamma Energy with Background",16384,0,16384,2048,0,16384);
     //TH2D *h_Ex_gamma_bg = new TH2D("h_Ex_gamma_bg","Excitation Energy vs gamma Energy Background",16384,0,16384,2048,0,16384);
     
-    //TH2D *h_labr_energy = new TH2D("h_labr_energy","labr Energy",32768,0,32768,30,1,31);
     //TH1D *h_labr_with_bg = new TH1D("h_labr_with_bg","labr with Background",32768,0,32768);
     //TH1D *h_labr_bg = new TH1D("h_labr_bg","labr Background",32768,0,32768);
     //TH2D *h_labr_gamma_gamma = new TH2D("h_labr_gamma_gamma","labr gamma-gamma",8192,0,32762,8192,0,32762);
     //TH3D *h_labr_time = new TH3D("h_labr_time","labr Time",20000,-1000,1000,64,1,65,30,1,31);
     //TH2D *h_labr_energy_time = new TH2D("h_labr_energy_time","labr Energy-Time",1024,0,1024,40000,-1000,1000);
-    //TH2D *h_labr_energy_time = new TH2D("h_labr_energy_time","labr Energy-Time",8192,0,8192,20000,-1000,1000);
     // ************************************************************************//
  
     // Possibility for reading in files with pre-defined graphical cuts - not used at present
@@ -177,120 +169,40 @@ void read_tree_96Mo(){
     for (Long64_t n=0; n<nrow; n++){      
         t->GetEntry(n); // Read event by event
 
-        // Delta E multiplicity
-        h_deDet_mult->Fill(deDet_mult);
+        h_deDet_energy->Fill(deDet_energy,deDet_ID-1);
         // E multiplicity
         h_eDet_mult->Fill(eDet_mult);
         // LaBr3 multiplicity
         h_LaBr3_mult->Fill(labr_mult);
+        
+        for (int i=0; i<eDet_mult; ++i) {
+            n_eDet++;
+			h_eDet_energy->Fill(eDet_energy[i],tel_ID-1);
+            h_eDet_time->Fill(eDet_time[i],tel_ID-1);
+            // Fill the "banana" matrices, one for each back and front detector combination
+            //std::cout << "E det ID = " << tel_ID << ", Delta E det ID = " << deDet_ID << std::endl;
+            const int id_f = (deDet_ID-1) % 8; // from here, the front strip has ID 0..7
+            string bananaName;
+            //TObject *obj = (TObject*)list->At(i);
+             //= deltaE_E_matrices[tel_ID][deDet_ID]->GetName();
+            //deltaE_E_matrices[tel_ID][id_f]->Fill(eDet_energy[i],deDet_energy);
 
-        // Fill the "banana" matrices, one for each back and front detector combination
-        //deltaE_E_matrices[tel_ID][deDet_ID]->Fill(eDet_energy,deDet_energy);
-
- /*       
-        for (int i=0; i<eDet_mult ; ++i) {
-                h_de_e_energy->Fill(eDet_energy[i],deDet_energy);
-                for (int j=0; j<labr_mult ; ++j) {
-                    if (labr_ID[j]==1) {
-                        h_labr_time->Fill(labr_time[j]);
-                        if(labr_time[j]>-9 && labr_time[j]<1){
-                            h_Ex_gamma_with_bg->Fill(labr_energy[j],eDet_energy[i]+deDet_energy);
-                        }
-                        else if(labr_time[j]>-65 && labr_time[j]<-55){
-                            h_Ex_gamma_bg->Fill(labr_energy[j],eDet_energy[i]+deDet_energy);
-                        }
-                    }
-                }
+            for (int j=0; j<labr_mult ; ++j){
+                h_LaBr3_energy->Fill(labr_energy[j],labr_ID[j]-1);
+                h_LaBr3_time->Fill(labr_time[j],labr_ID[j]-1);
             }
-            */
         
-            //h_deDet_energy->Fill(deDet_energy,deDet_ID);
-            //for (int i=0; i<eDet_mult ; ++i) {
-                //if (cut_ban->IsInside(eDet_energy[i],deDet_energy) && cut_eDet->IsInside(eDet_energy[i],eDet_time[i]) && (Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406)<6551) {
-                     //h_deDet_energy->Fill(deDet_energy);
-                     //h_eDet_energy->Fill(eDet_energy[i],tel_ID);
-                     //h_eDet_time->Fill(eDet_time[i]);
-                     //h_de_e_energy->Fill(eDet_energy[i],deDet_energy);
-                     //h_eDet_energy_time->Fill(eDet_energy[i],eDet_time[i]);
-                     //h_eDet_mult->Fill(eDet_mult);
-                     //break;
-                     //for (int j=0; j<labr_mult ; ++j) {
-                         //h_labr_mult->Fill(labr_mult);
-                         //h_labr_energy_time->Fill(labr_energy[j],labr_time[j]);
-                         //h_labr_time->Fill(labr_time[j],deDet_ID,labr_ID[j]);
-                         //h_labr_energy->Fill(labr_energy[j],labr_ID[j]);
-                         //if (labr_ID[j]==1) {
-                             //h_labr_time->Fill(labr_time[j]);
-                             //h_labr_energy->Fill(labr_energy[j]);
-                             //h_labr_energy_time->Fill(labr_energy[j],labr_time[j]);
-                         //}
-                         //if (cut_Ex->IsInside(labr_energy[j],Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406)/* && cut_labr->IsInside(labr_energy[j],labr_time[j])*/) {
-                             //h_labr_energy_time->Fill(labr_energy[j],labr_time[j]);
-                             //h_labr_energy->Fill(labr_energy[j]);
-                         //}
-                         /*
-                         //if (labr_time[j]>142 && labr_time[j]<172) { //for delayed_p_cut
-                         if ((labr_time[j]>110 && labr_time[j]<130)||(labr_time[j]>150 && labr_time[j]<175)||(labr_time[j]>195 && labr_time[j]<220)||(labr_time[j]>240 && labr_time[j]<265)||(labr_time[j]>285 && labr_time[j]<310)||(labr_time[j]>330 && labr_time[j]<355)||(labr_time[j]>375 && labr_time[j]<400)||(labr_time[j]>420 && labr_time[j]<445)||(labr_time[j]>465 && labr_time[j]<490)) { //for delayed_d_cut
-                             //h_labr_energy->Fill(labr_energy[j]);
-                             h_labr_energy->Fill(labr_energy[j],labr_ID[j]);
-                         }
-                         */
-                         
-                         //if (cut_labr->IsInside(labr_energy[j],labr_time[j]) && labr_energy[j]>305 && labr_energy[j]<=(Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406)) {
-                             //h_Ex_gamma_with_bg->Fill(labr_energy[j],Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406);
-                             //h_labr_energy->Fill(labr_energy[j],labr_ID[j]);
-                             //h_labr_energy->Fill(labr_energy[j]);
-                             //h_labr_with_bg->Fill(labr_energy[j]);
-                         //}
-                         //else if (cut_bg->IsInside(labr_energy[j],labr_time[j]) && labr_energy[j]>305 && labr_energy[j]<=(Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406)) {
-                             //h_Ex_gamma_bg->Fill(labr_energy[j],Ex_coeff[(deDet_ID-tel_ID)%8][0]+(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][1]+(eDet_energy[i]+deDet_energy)*(eDet_energy[i]+deDet_energy)*Ex_coeff[(deDet_ID-tel_ID)%8][2]-215.406);
-                         //}
-                         
-                     //}
-                    
-                    n_eDet++;
-                //}
-                //h_eDet_energy->Fill(eDet_energy[i]);
-                //h_eDet_time->Fill(eDet_time[i]);
-                //h_de_e_energy->Fill(eDet_energy[i],deDet_energy);
-            //}
-            if (n_eDet>1) {
-                n_eDet_bg=n_eDet_bg+n_eDet-1;
+            if(n%1000000==0){
+                std::cout << ".";
+                std::cout.flush();
             }
-            n_in_cut=n_in_cut+n_eDet;
-            n_eDet=0;
-        //}
-        
-        /*h_deDet_ID->Fill(deDet_ID);
-        h_tel_ID->Fill(tel_ID);
-        h_deDet_energy->Fill(deDet_energy);
-        h_deDet_timestamp->Fill(deDet_timestamp/ns_to_seconds);
- 
-        h_eDet_mult->Fill(eDet_mult);
-        for (int i=0; i<eDet_mult ; ++i) {
-            h_eDet_energy->Fill(eDet_energy[i]);
-            h_eDet_time->Fill(eDet_time[i]);
-        }
-        
-        h_labr_mult->Fill(labr_mult);
-        for (int i=0; i<labr_mult ; ++i) {
-            h_labr_ID->Fill(labr_ID[i]);
-            h_labr_ring->Fill(labr_ring[i]);
-            h_labr_energy->Fill(labr_energy[i]);
-            h_labr_time->Fill(labr_time[i]);
-        }*/
-        
-        if(n%1000000==0){
-            std::cout << ".";
-            std::cout.flush();
-        }//*/
-        
+        } 
     }
     
     std::cout << endl;
     std::cout << "Total number of events:           " << nrow << endl;
-    std::cout << "Number of events inside the cut:  " << n_in_cut << endl;
-    std::cout << "Number of eDet background events: " << n_eDet_bg << endl;
+    //std::cout << "Number of events inside the cut:  " << n_in_cut << endl;
+    //std::cout << "Number of eDet background events: " << n_eDet_bg << endl;
     
     
     //TCanvas *c_de_e_energy = new TCanvas("c_de_e_energy","deDet eDet Energy",1);
@@ -302,28 +214,26 @@ void read_tree_96Mo(){
     //if (!(h_Ex_gamma->GetSumw2N() > 0)) h_Ex_gamma->Sumw2(kTRUE);
     //h_Ex_gamma->Add(h_Ex_gamma_bg, -1.0);
     
-    //ostringstream bananaName = deltaE_E_matrices[tel_ID][deDet_ID]->GetName();
     
-    TFile *outputFile = new TFile("Mo96_plain.root","update");
-    h_deDet_mult->Write("h_deDet_mult",TObject::kOverwrite);
+    TFile *outputFile = new TFile("Mo96_plain.root","recreate");
     h_eDet_mult->Write("h_eDet_mult",TObject::kOverwrite);
     h_LaBr3_mult->Write("h_LaBr3_mult",TObject::kOverwrite);
+    h_deDet_energy->Write("h_deDet_energy",TObject::kOverwrite);
+    h_eDet_energy->Write("h_eDet_energy",TObject::kOverwrite);
+    h_eDet_time->Write("h_eDet_time",TObject::kOverwrite);
+    h_LaBr3_energy->Write("h_LaBr3_energy",TObject::kOverwrite);
+    h_LaBr3_time->Write("h_LaBr3_time",TObject::kOverwrite);
     //deltaE_E_matrices[tel_ID][deDet_ID]->Write(bananaName,TObject::kOverwrite);
 
-    //h_deDet_energy->Write("h_deDet_energy_peak4");
-    //h_eDet_energy->Write("h_eDet_energy",TObject::kOverwrite);
     //h_eDet_time->Write("h_eDet1_time_gs_19F_cut");
-    //h_labr_time->Write("h_labr_time",TObject::kOverwrite);//,TObject::kOverwrite
     //h_labr_energy->Write("h_labr_energy",TObject::kOverwrite);
     //h_labr_gamma_gamma->Write("h_labr_gamma_gamma_peak2",TObject::kOverwrite);
     //h_de_e_energy->Write("h_de_e_energy",TObject::kOverwrite);
     //h_eDet_energy_time->Write("h_eDet_energy_time_Ex_cut",TObject::kOverwrite);
     //h_labr_energy_time->Write("h_labr_energy_time",TObject::kOverwrite);
-    //h_eDet_mult->Write("h_eDet_mult_t_cut",TObject::kOverwrite);
     //h_Ex_gamma_with_bg->Write("h_Ex_gamma_with_bg",TObject::kOverwrite);
     //h_Ex_gamma_bg->Write("h_Ex_gamma_bg",TObject::kOverwrite);
     //h_Ex_gamma->Write("h_Ex_gamma",TObject::kOverwrite);
-    //h_labr_mult->Write("h_labr_mult",TObject::kOverwrite);
     outputFile->Close();
     
 }
